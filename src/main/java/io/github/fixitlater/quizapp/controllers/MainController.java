2c9de71bf224eaa4dcbfe9f743bdfda52323a259
Ewa <efciajed@gmail.com>
2019-03-17 20:02:40 +0100

    radio buttons and evaluate results (fixed 4)

--- src/main/java/io/github/fixitlater/quizapp/controllers/MainController.java
+++ src/main/java/io/github/fixitlater/quizapp/controllers/MainController.java
@@ -1,24 +1,30 @@
 package io.github.fixitlater.quizapp.controllers;
 
 import io.github.fixitlater.quizapp.dtos.QuestionDto;
+import io.github.fixitlater.quizapp.services.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class MainController {
 
     private RestTemplate restTemplate;
+    private QuestionService questionService;
 
     @Autowired
-    public MainController(RestTemplateBuilder restTemplateBuilder) {
+    public MainController(RestTemplateBuilder restTemplateBuilder, QuestionService questionService) {
         this.restTemplate = restTemplateBuilder.build();
+        this.questionService = questionService;
     }
 
     @GetMapping (value = {"/","/index","/home"})
@@ -41,6 +47,13 @@ public class MainController {
         List<QuestionDto> questions = Arrays.asList(questionDtos);
         model.addAttribute("questions", questions);
         return "quiz/allQuestions";
+    }
+
+    @PostMapping("/quiz/results")
+    public String evaluateAnswers(@RequestParam Map<String, String> allParameters, Model model){
+        int correctAnswers = questionService.evaluateAnswers(allParameters);
+        model.addAttribute("correctAnswers", correctAnswers);
+        return "quiz/evaluate";
     }
 
 }
--- src/main/java/io/github/fixitlater/quizapp/services/QuestionService.java
+++ src/main/java/io/github/fixitlater/quizapp/services/QuestionService.java
@@ -1,7 +1,17 @@
 package io.github.fixitlater.quizapp.services;
 
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
 /**
  * Created by ej on 17/03/2019.
  */
+@Service
 public class QuestionService {
+
+    public int evaluateAnswers(Map<String, String> allParameters){
+
+        return 4;
+    }
 }
