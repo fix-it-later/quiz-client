9f4aecd2beb01aa6c89c0c2a648c9164e4a3d7db
arturzaczek1 <artur.zaczek@tlen.pl>
2019-03-18 23:56:14 +0100

    added list of categories and languages from api

--- src/main/java/io/github/fixitlater/quizapp/controllers/QuizController.java
+++ src/main/java/io/github/fixitlater/quizapp/controllers/QuizController.java
@@ -1,22 +1,44 @@
 package io.github.fixitlater.quizapp.controllers;
 
+
+import io.github.fixitlater.quizapp.services.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
+
 @Controller
 public class QuizController {
 
-    @GetMapping ("quiz/menu")
-    public String getQuizMenu (){
+    private QuizService quizService;
+
+    @Autowired
+    public QuizController(QuizService quizService){
+        this.quizService = quizService;
+    }
+
+
+    @GetMapping("quiz/menu")
+    public String getQuizMenu(Model model) {
+        model.addAttribute("categories", quizService.getCategoryList());
+        model.addAttribute("languages", quizService.getLanguageList());
+        quizService.getLanguageList().forEach(a-> System.out.println(a));
         return "quiz/quizMenu";
     }
 
-    @PostMapping("/quiz/challenge ")
-    public String showParamQuiz(@RequestParam String category, @RequestParam String lang){
+    @PostMapping("/quiz/challenge")
+    public String showParamQuiz(@RequestParam String category, @RequestParam String lang) {
         System.out.println(category + " " + lang);
+        return "index";
+    }
+
+    @GetMapping("/quiz/test")
+    public String showCategories(Model model) {
+
         return "index";
     }
 
